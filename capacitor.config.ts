a446ece332e49c34a52c9b8185e94407f5b38993
barbar-binks <gerardo.barbier@gmail.com>
2024-10-07 16:36:18 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'demo-maps',
+  webDir: 'www'
+};
+
+export default config;
