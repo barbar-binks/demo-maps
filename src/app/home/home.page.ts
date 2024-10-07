dc5a6acadbc1a9278402a6b3fec40d1cb31b2da2
barbar-binks <gerardo.barbier@gmail.com>
2024-10-07 17:12:33 -0600

    demo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GoogleMap } from '@capacitor/google-maps';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+
+  maps: string = ''; // PASTE YOUR GOOGLE MAPS API KEY HERE
+  mapId: string; // PASTE YOUR MAP JAVASCRIPT MAP ID HERE
+  map: GoogleMap;
 
   constructor() {}
 
+  async ngOnInit(): Promise<void> {
+      const mapRef = document.getElementById('map');
+      this.map = await GoogleMap.create({
+        id: 'test-map',
+        element: mapRef!,
+        apiKey: this.maps,
+        config: {
+          mapId: this.mapId,
+          center: {
+            lat: 13.70146,
+            lng: -89.22443,
+          },
+          zoom: 20,
+          //disableDefaultUI: true, // this causes the issue
+        }
+        
+      });
+  }
+
+  
 }
